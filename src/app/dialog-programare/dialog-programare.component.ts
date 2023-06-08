4f8645afa8a58fcd6b0d0bc5fc4cc909afe536dd
Andra Frunza <87007567+AndraFrunza@users.noreply.github.com>
2023-06-08 20:56:28 +0300

    Dialog improvements

--- src/app/dialog-programare/dialog-programare.component.ts
+++ src/app/dialog-programare/dialog-programare.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-programare',
   templateUrl: './dialog-programare.component.html',
-  styleUrls: ['./dialog-programare.component.css']
+  styleUrls: ['./dialog-programare.component.css'],
 })
 export class DialogProgramareComponent {
+  constructor(private dialogRef: MatDialogRef<DialogProgramareComponent>) {}
 
+  closeDialog() {
+    this.dialogRef.close();
+  }
 }
